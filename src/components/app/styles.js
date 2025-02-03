110084b8458a478febc469f493de84998972bab7
VladissGernet <skeimorr@gmail.com>
2025-02-03 12:07:29 +0400

    feat: добавил sticky footer

--- src/components/app/styles.js
+++ src/components/app/styles.js
@@ -1,23 +1,31 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  html {
-    height: 100%;
-  }
-
   body,
   html {
     margin: 0;
+    height: 100%;
   }
 
   body {
     position: relative;
-    min-height: 100%;
     font-family: ${(props) => props.theme.fontFamily};
     font-size: ${(props) => props.theme.fontSizeDefault};
     line-height: 27px;
     font-weight: 400;
     color: ${(props) => props.theme.colorBlackForText};
+
     background-color: grey;
+  }
+
+${'' /* Sticky footer */}
+  body>div {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  main {
+    flex-grow: 1;
   }
 `;
