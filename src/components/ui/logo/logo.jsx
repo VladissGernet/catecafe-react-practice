83ea9ede5cb6475b0cbb51b43b21928c5377701b
VladissGernet <skeimorr@gmail.com>
2025-02-04 10:12:01 +0400

    feat: добавил лого

--- src/components/ui/logo/logo.jsx
+++ src/components/ui/logo/logo.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { useMatch, NavLink } from 'react-router-dom';
+import { useMatch } from 'react-router-dom';
+import { ReactComponent as LogoImage } from '/src/assets/logo.svg';
+
+import {
+  LogoText,
+  StyledLogoNavLink,
+  StyledLogoSpan,
+} from './styled';
 
 const Logo = () => {
   const isIndexPage = useMatch({ path: '/' });
 
+  const LogoContent = () => (
+    <>
+      <LogoImage
+        width="44"
+        height="44"
+      />
+      <LogoText>Котокафе</LogoText>
+    </>
+  );
+
   return isIndexPage
-  ? (<span>Logo</span>)
-  : (<NavLink to="/">Logo</NavLink>)
+  ? (<StyledLogoSpan><LogoContent /></StyledLogoSpan>)
+  : (<StyledLogoNavLink to="/"><LogoContent /></StyledLogoNavLink>)
 };
 
 export { Logo };
