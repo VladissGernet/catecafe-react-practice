aa1afbceee0f26b8378a57a74caa30dc3c4e0b8f
VladissGernet <skeimorr@gmail.com>
2025-02-13 20:47:17 +0400

    добавил заголовки

--- src/components/blocks/form-order/form-order.jsx
+++ src/components/blocks/form-order/form-order.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { buyOptions } from '/src/mocks/buy-options.js';
 
 import { Form } from 'src/components/blocks/form-order/styled';
+import { Title, TitleSizes } from 'src/components/ui/title/title';
 import { Duration } from 'src/components/blocks/duration/duration';
 
 const FormOrder = () => {
@@ -11,11 +12,13 @@ const FormOrder = () => {
 
   return (
     <Form>
+      <Title level={3} size={TitleSizes.SMALL}>Продолжительность (часов)</Title>
       <Duration
         onChange={(el) => setDuration(el.target.value)}
         durationOptions={buyOptions.durationOptions}
         durationValue={duration}
       />
+      <Title level={3} size={TitleSizes.SMALL}>Тип билета</Title>
     </Form>
   )
 };
--- src/components/blocks/form-order/styled.js
+++ src/components/blocks/form-order/styled.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { DurationUl } from '/src/components/blocks/duration/styled';
+
 const Form = styled.form`
   background-color: ${(props) => props.theme.colorLight};
   padding: 20px;
@@ -7,6 +9,11 @@ const Form = styled.form`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   max-width: 50%;
+
+  ${DurationUl} {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
 `;
 
 export { Form };
